18b74899865ab16526c26a4096de9c767de218ff
CaioMF <caio.fagundes@mercadolivre.com>
2021-06-30 17:15:00 -0300

    Delete src directory

--- src/br/com/meli/day3/People/Previous.java
+++
@@ -1,5 +0,0 @@
-package br.com.meli.day3.People;
-
-public interface Previous<T> {
-    int previousA(T t);
-}
--- src/br/com/meli/day4/LaCiqui/Firework.java
+++
@@ -1,7 +0,0 @@
-package br.com.meli.day4.LaCiqui;
-
-public interface Firework {
-
-    void explosion();
-
-}
